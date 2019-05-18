72e35f892afb2b074a2d5a70924d445d5de0c961
Leila <nb.leila10@gmail.com>
2019-05-18 14:53:31 -0500

    Add some comments to know what to add to typeDefs.js

--- resolvers/users/usersResolvers.js
+++ resolvers/users/usersResolvers.js
@@ -11,50 +11,35 @@ const authenticated = next => (root, args, ctx, info) => {
   return next(root, args, ctx, info);
 };
 
-/*
-In typeDefs:
-type Query: {
-  getCurrentUser: User
-  getUsers: [User!]!
-  getUserById(userId: ID!): User!
-  getUserBy(filter: String!, value: String!): [User!]
-}
-type Mutation: {
-  addUser(input: UserInput!): User!
-  deleteUser(userId: ID!): Int!
-  updateUser(userId: ID!, input: UpdateUserInput!): User!
-}
-input UserInput {
-  firstName: String!
-  lastName: String!
-  username: String!
-  email: String!
-  userType: String!
-  calorieGoal: Int!
-  weight: Int!
-}
-input UpdateUserInput {
-  firstName: String
-  lastName: String
-  username: String
-  email: String
-  userType: String
-  calorieGoal: Int
-  weight: Int
-}
-*/
+// Add in typeDefs.js
+// type Query {
+//   getUserBy(filter: String!, value: String!): [User!]
+// }
+// type Mutation {
+//  addUser(input: UserInput!): User!
+//  updateUser(userId: ID!, input: UpdateUserInput!): User!
+// }
+// input UserInput {
+//   firstName: String!
+//   lastName: String!
+//   username: String!
+//   email: String!
+//   userType: String!
+//   calorieGoal: Int!
+//   weight: Int!
+// }
+// input UpdateUserInput {
+//   firstName: String
+//   lastName: String
+//   username: String
+//   email: String
+//   userType: String
+//   calorieGoal: Int
+//   weight: Int
+// }
 
 module.exports = {
   Query: {
-    // Add in typeDefs.js
-    // type Query {
-    //   getCurrentUser: User
-    //   getUsers: [User]
-    //   getUserById(userId: ID!): User
-    //   getFoodEntries(userId: ID!): FoodEntry!
-    //   getExerciseEntries(userId: ID!): ExerciseEntry!
-    // }
-
     getCurrentUser: authenticated((root, args, ctx) => ctx.currentUser),
 
     getUsers: async (root, args, ctx) => {
@@ -68,21 +53,20 @@ module.exports = {
     },
 
     getUserBy: async (root, args, ctx) => {
-      console.log("in getUserBy ", args);
       const user = await User.findBy({ [args.filter]: args.value });
       console.log({ user });
       return user;
+    },
+
+    getFoodEntriesByUserId: async (root, args, ctx) => {
+      const entries = await FoodEntry.findBy({ user_id: args.userId });
+      return entries;
+    },
+
+    getExerciseEntriesByUserId: async (root, args, ctx) => {
+      const entries = await ExerciseEntry.findBy({ user_id: args.userId });
+      return entries;
     }
-
-    // getFoodEntries: async (root, args, ctx) => {
-    //   const entries = await FoodEntry.findBy({ user_id: args.userId });
-    //   return entries;
-    // },
-
-    // getExerciseEntries: async (root, args, ctx) => {
-    //   const entries = await ExerciseEntry.findBy({ user_id: args.userId });
-    //   return entries;
-    // }
   },
 
   User: {
