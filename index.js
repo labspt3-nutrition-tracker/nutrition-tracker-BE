bcd9f4860dd83cfb2f00f6fb2ed7fbd116dc5eb1
Leila <nb.leila10@gmail.com>
2019-05-21 21:59:46 -0500

    Fix bug

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require("./server");
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-    console.log(`Server is listening on ${url}`);
-
+  console.log(`Server is listening on ${url}`);
+});
